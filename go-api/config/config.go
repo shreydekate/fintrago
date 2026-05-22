610b8296aad5cefb580c106f752f33b00568ed8f
reecebaileyy <reece.bailey8857@yahoo.com>
2026-05-21 18:21:24 -0700

    Reece Phase 1 Scaffold

--- go-api/config/config.go
+++ go-api/config/config.go
@@ -2,21 +2,30 @@ package config
 
 import (
 	"os"
+
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port string
-	DatabaseURL string
-	JWTSecret string
+	Port         string
+	DatabaseURL  string
+	JWTSecret    string
 	AIServiceURL string
 }
 
 func Load() *Config {
 	godotenv.Load()
-	return &Config {
-		Port: getEnv("PORT", "8080"),
-		DatabaseURl: getEnv("DATABASE_URL", ""),
-		JWTSecret: 
+	return &Config{
+		Port:         getEnv("PORT", "8080"),
+		DatabaseURL:  getEnv("DATABASE_URL", ""),
+		JWTSecret:    getEnv("JWT_SECRET", ""),
+		AIServiceURL: getEnv("AI_SERVICE_URL", "http://localhost:8000"),
 	}
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
 }
